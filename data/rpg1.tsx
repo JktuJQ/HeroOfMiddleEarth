95f0f996de2259d3103d16f46072bdaddf2157ba
Zverinus <75039044+Zverinus@users.noreply.github.com>
2021-01-14 23:31:50 +0300

    working build v1.1

---
+++ data/rpg1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rpg1" tilewidth="32" tileheight="32" tilecount="341" columns="11">
+ <image source="../../hero_of_middleearth/data/ApplicationFrameHost_eG8xADEEsP.png" trans="082152" width="362" height="1023"/>
+</tileset>
---
+++ data/rpg4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rpg4" tilewidth="32" tileheight="32" tilecount="256" columns="8">
+ <image source="../../hero_of_middleearth/data/ApplicationFrameHost_X5XUhAqrwZ.png" trans="082152" width="256" height="1024"/>
+</tileset>
