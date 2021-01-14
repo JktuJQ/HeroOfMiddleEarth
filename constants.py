e10c5e541159adeed91a4b59335dc34ce47d952a
VladimirSpe <75093117+VladimirSpe@users.noreply.github.com>
2021-01-15 00:16:39 +0300

    Add files via upload

--- constants.py
+++ constants.py
@@ -43,11 +43,15 @@ LEVELS = ['level_0.tmx', 'level_1.tmx', 'level_2.tmx']
 INVENTORY_PROP = {'gun': {'type of shooting': 'line', 'range': 200, 'damage': 20,
                           'animation': bullet_animation("data/images/bullet.png"),
                           'cooldown': 500},
-                  'sword': {'type of shooting': 'circle', 'range': 50, 'damage': 50, 'cooldown': 500,
-                            'animation': bullet_animation("data/images/bullet.png")},
+                  'sword': {'type of shooting': 'line', 'range': 50, 'damage': 50, 'cooldown': 500,
+                            'animation': bullet_animation("data/images/sword.png")},
                   'bow': {'type of shooting': 'line', 'range': 250,
                           'damage': 40, 'cooldown': 1000,
-                          'animation': bullet_animation("data/images/arrow.png")}}
+                          'animation': bullet_animation("data/images/arrow.png")},
+                  'zombie_ball': {'type of shooting': 'line', 'range': 100,
+                                  'damage': 40, 'cooldown': 1000,
+                                  'animation': bullet_animation("data/images/zombie_ball.png")}
+                  }
 
 RIGHT_HERO = [bullet_animation("block/Player/Mag/Right/mag_right_s.png"),
               bullet_animation("block/Player/Mag/Right/mag_right_r.png"),
@@ -83,6 +87,12 @@ MOBS = {'archer': {'type_gun': 'bow', 'hp': 50, 'speed': 130,
                                     bullet_animation("block/Player/Mobs/Warrior/warrior_run_r.png")],
                     'animation_l': [bullet_animation("block/Player/Mobs/Warrior/warrior_stay_l.png"),
                                     bullet_animation("block/Player/Mobs/Warrior/warrior_run_l.png")]
+                    },
+        'zombie': {'type_gun': 'zombie_ball', 'hp': 100, 'speed': 100,
+                    'animation_r': [bullet_animation("block/Player/Mobs/Zombie/zombie_stay_r.png"),
+                                    bullet_animation("block/Player/Mobs/Zombie/zombie_run_r.png")],
+                    'animation_l': [bullet_animation("block/Player/Mobs/Zombie/zombie_stay_l.png"),
+                                    bullet_animation("block/Player/Mobs/Zombie/zombie_run_r.png")]
                     }}
 
 TILE_SIZE = 32
